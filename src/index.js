8ca3c6baacf77bf5acc3587233638f6f93a39206
Dejan Rabic <dejan.rabic@3fs.si>
2020-04-18 19:36:21 +0200

    Add selected feature to special layer

--- src/index.js
+++ src/index.js
@@ -3,21 +3,27 @@ import GeoJSON from 'ol/format/GeoJSON';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import {defaults as defaultControls, ScaleLine} from 'ol/control';
+import { click } from 'ol/events/condition';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import VectorSource from 'ol/source/Vector';
 import VectorLayer from 'ol/layer/Vector';
+import Select from 'ol/interaction/Select';
 import Style from 'ol/style/Style';
 import Stroke from 'ol/style/Stroke';
-import Fill from 'ol/style/Fill';
-import { Overlay } from 'ol';
+import { Overlay, Collection } from 'ol';
 
 import obcineJson from '../data/obcine.geojson';
+import Fill from 'ol/style/Fill';
 
 // COSTANTS
 const POPUP_ID = "popup";
 const MAP_CENTER = [1655360.12, 5793576.89];
 const MESTNA = "D";
+
+const selectedCollection = new Collection([], {
+    unique: true,
+});
 
 // LAYERS
 const tileLayer = new TileLayer({
@@ -32,14 +38,10 @@ const obcineSrc = new VectorSource({
 const obcineDefaultStyle = new Style({
     stroke: new Stroke({
         color: 'blue',
-        width: 2,
+        width: 1,
     }),
-});
-
-const obcineSelectedStyle = new Style({
-    stroke: new Stroke({
-        color: 'red',
-        width: 2,
+    fill: new Fill({
+        color: 'rgba(255, 255, 255, 0.1)',
     }),
 });
 
@@ -48,9 +50,26 @@ const obcineLayer = new VectorLayer({
     style: obcineDefaultStyle,
 });
 
+const selectedSrc = new VectorSource({
+    features: selectedCollection,
+});
+
+const selectedStyle = new Style({
+    stroke: new Stroke({
+        color: 'red',
+        width: 2,
+    }),
+});
+
+const selectedLayer = new VectorLayer({
+    source: selectedSrc,
+    style: selectedStyle,
+})
+
 const layers = [
     tileLayer,
     obcineLayer,
+    selectedLayer,
 ];
 
 // CREATE MAP
@@ -73,8 +92,9 @@ const popup = new Overlay({
     element: document.getElementById(POPUP_ID),
 });
 
-let counter = 1;
 function mapClickHandler(evt) {
+    selectedSrc.clear();
+
     let element = popup.getElement();
     let coordinate = evt.coordinate;
     let obcina = getObcinaFeature(coordinate);
@@ -95,8 +115,8 @@ function mapClickHandler(evt) {
     `;
 
     popup.setPosition(coordinate);
+    selectedSrc.addFeature(obcina);
 }
-
 
 function getObcinaFeature(coordinate) {
     const features = obcineSrc.getFeaturesAtCoordinate(coordinate);
