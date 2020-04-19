33c31390fbdd5b803384a1b44729a0e7fbd6f46e
Dejan Rabic <dejan.rabic@3fs.si>
2020-04-19 21:09:39 +0200

    Display user location

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
 import 'ol/ol.css';
+
+import Circle from 'ol/style/Circle';
+import Fill from 'ol/style/Fill';
 import GeoJSON from 'ol/format/GeoJSON';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import {defaults as defaultControls, ScaleLine} from 'ol/control';
-import { click } from 'ol/events/condition';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import Point from 'ol/geom/Point';
 import VectorSource from 'ol/source/Vector';
 import VectorLayer from 'ol/layer/Vector';
-import Select from 'ol/interaction/Select';
 import Style from 'ol/style/Style';
 import Stroke from 'ol/style/Stroke';
-import { Overlay, Collection } from 'ol';
+import { Overlay, Collection, Feature, Geolocation } from 'ol';
+import {transform} from 'ol/proj';
 
-import obcineJson from '../data/obcine.geojson';
-import Fill from 'ol/style/Fill';
+import obcineJson from './data/obcine.geojson';
 
 // COSTANTS
 const POPUP_ID = "popup";
 const MAP_CENTER = [1655360.12, 5793576.89];
 const MESTNA = "D";
+
+const elObcinaAvailable = document.querySelector('#position > .available');
+const elObcinaOutside = document.querySelector('#position > .outside');
+const elObcinaUnavailable = document.querySelector('#position > .unavailable');
+const elObcinaName = document.getElementById('obcina-name');
+const elObcinaLat = document.getElementById('loc-lat');
+const elObcinaLon = document.getElementById('loc-lon');
 
 const selectedCollection = new Collection([], {
     unique: true,
@@ -39,6 +48,7 @@ const obcineDefaultStyle = new Style({
     stroke: new Stroke({
         color: 'blue',
         width: 1,
+        lineDash: [2, 5],
     }),
     fill: new Fill({
         color: 'rgba(255, 255, 255, 0.1)',
@@ -64,12 +74,23 @@ const selectedStyle = new Style({
 const selectedLayer = new VectorLayer({
     source: selectedSrc,
     style: selectedStyle,
-})
+});
+
+const locationSrc = new VectorSource();
+
+const locationStyles = generateLocationStyles();
+let locationStyleIdx = 0;
+
+const locationLayer = new VectorLayer({
+    source: locationSrc,
+    style: locationStyles[0],
+});
 
 const layers = [
     tileLayer,
     obcineLayer,
     selectedLayer,
+    locationLayer,
 ];
 
 // CREATE MAP
@@ -130,3 +151,104 @@ function getObcinaFeature(coordinate) {
 
 map.addOverlay(popup);
 map.on('click', mapClickHandler);
+
+// SHOW LOCATION
+const userLocation = new Feature();
+locationSrc.addFeature(userLocation);
+
+const geolocation = new Geolocation({
+    tracking: true,
+    trackingOptions: { enableHighAccuracy: true },
+    projection: map.getView().getProjection(),
+});
+
+geolocation.on('change:position', (evt) => {
+    const coordinate = geolocation.getPosition();
+    const point = coordinate ? new Point(coordinate) : null;
+    userLocation.setGeometry(point);
+
+    updateObcinaInfo(coordinate);
+});
+
+geolocation.on('error', err => {
+    locationSrc.clear();
+    setLocationMessageErr();
+});
+
+function updateObcinaInfo(coordinate) {
+    let obcina = getObcinaFeature(coordinate);
+
+    if (obcina == null) {
+        setLocationMessageOut();
+        return;
+    }
+
+    let name = obcina.get('OB_UIME');
+    let wgsCoord = transform(coordinate, 'EPSG:3857', 'EPSG:4326');
+
+    setLocationMessageOk(name, wgsCoord);
+}
+
+// ANIMATE LOCATION MARKER
+function generateLocationStyles() {
+    const styles = [];
+
+    for (let idx = 12; idx > 2; idx--) {
+        styles.push(new Style({
+            image: new Circle({
+                radius: idx,
+                fill: new Fill({color: 'white'}),
+                stroke: new Stroke({
+                    color: 'red',
+                    width: 1,
+                }),
+            }),
+        }));
+    }
+
+    return styles;
+}
+
+function locationStyleAnimationStep() {
+    const nextIdx = locationStyleIdx++ % (locationStyles.length - 1);
+    const nextStyle = locationStyles[nextIdx];
+    locationLayer.setStyle(nextStyle);
+}
+
+function locationStyleAnimate() {
+    locationStyleAnimationStep();
+    setTimeout(locationStyleAnimate, 250);
+}
+
+locationStyleAnimate();
+
+// DOM MANIPULATION
+function setLocationMessageOk(obcinaName, coordinate) {
+    elObcinaName.textContent = obcinaName;
+    elObcinaLat.textContent = `${coordinate[1]}N`;
+    elObcinaLon.textContent = `${coordinate[0]}E`;
+
+    display(elObcinaAvailable);
+    hide(elObcinaOutside);
+    hide(elObcinaUnavailable);
+}
+
+function setLocationMessageOut() {
+    hide(elObcinaAvailable);
+    display(elObcinaOutside);
+    hide(elObcinaUnavailable);
+}
+
+function setLocationMessageErr() {
+    hide(elObcinaAvailable);
+    hide(elObcinaOutside);
+    display(elObcinaUnavailable);
+}
+
+function display(el) {
+    el.classList.remove("hide");
+}
+
+function hide(el) {
+    el.classList.add("hide");
+}
